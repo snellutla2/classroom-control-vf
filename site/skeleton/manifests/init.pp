aa56580c7789a9569b93fbeb9ac4c9eada50ec11
snellutla2 <sampath.k.nellutla@accenture.com>
2016-04-05 14:34:54 -0500

    Update init.pp
    added .bashrc file

--- site/skeleton/manifests/init.pp
+++ site/skeleton/manifests/init.pp
@@ -2,6 +2,13 @@ class skeleton{
 file {
   '/etc/skel':
   ensure => directory,
-  
-  }
+  owner = > 'root',
+  mode => '0755',
+    }
+file {'/etc/skel/.bashrc':
+ensure => file,
+owner => 'root',
+group => '0644',
+source => 'puppet:///modules/skeleton/bashrc',
+}
 }
